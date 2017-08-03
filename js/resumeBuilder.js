11d1773f45bff443bedc9cb7a48320bcdd97577d
crazymaza <dx2389@gmail.com>
2017-08-03 22:59:49 +0300

    Add files via upload

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,16 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+//Меняем в переменной HTMLheaderName %data% на Игорь.
+ var formName = HTMLheaderName.replace("%data%", "Игорь Вальнев");
+
+ //Создаем переменную и с помощью нее меняем значение в другой переменной
+ var role = "Web Developer";
+ var formRole = HTMLheaderRole.replace("%data%", role);
+
+//Выбираем блок с ID header и вставляем в него нужные переменные
+$("#header").prepend(formRole);
+$("#header").prepend(formName);
+ 
+
